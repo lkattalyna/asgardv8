cd69bb802eb6378f6990b1925b030980495e6cca
erika <erikajgq@algartechcol.onmicrosoft.com>
2024-05-23 11:58:34 -0500

    mostrar la tabla al dar click en la lupa

--- resources/views/customer/customerDictionary.blade.php
+++ resources/views/customer/customerDictionary.blade.php
@@ -146,187 +146,189 @@
 @stop
 
 @section('js')
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            document.getElementById('agregar').addEventListener('click', function() {
-                var service = document.getElementById('service').value.trim();
-                if (service) {
-                    // Crear un nuevo div con la clase input-group
-                    var newInputGroup = document.createElement('div');
-                    newInputGroup.className = 'input-group mb-2';
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('agregar').addEventListener('click', function() {
+            var service = document.getElementById('service').value.trim();
+            if (service) {
+                // Crear un nuevo div con la clase input-group
+                var newInputGroup = document.createElement('div');
+                newInputGroup.className = 'input-group mb-2';
 
-                    // Crear el input visible, deshabilitado
-                    var newInput = document.createElement('input');
-                    newInput.setAttribute('type', 'text');
-                    newInput.setAttribute('name', 'valores_agregados[]');
-                    newInput.setAttribute('value', service);
-                    newInput.setAttribute('class', 'form-control');
-                    newInput.setAttribute('disabled', 'disabled');
+                // Crear el input visible, deshabilitado
+                var newInput = document.createElement('input');
+                newInput.setAttribute('type', 'text');
+                newInput.setAttribute('name', 'valores_agregados[]');
+                newInput.setAttribute('value', service);
+                newInput.setAttribute('class', 'form-control');
+                newInput.setAttribute('disabled', 'disabled');
 
-                    // Crear el botón de eliminar
-                    var deleteButton = document.createElement('button');
-                    deleteButton.className = 'btn btn-sm btn-danger ml-2';
-                    deleteButton.innerText = 'Eliminar';
-                    deleteButton.type = 'button';
-                    deleteButton.onclick = function() {
-                        eliminarValor(newInputGroup);
-                    };
+                // Crear el botón de eliminar
+                var deleteButton = document.createElement('button');
+                deleteButton.className = 'btn btn-sm btn-danger ml-2';
+                deleteButton.innerText = 'Eliminar';
+                deleteButton.type = 'button';
+                deleteButton.onclick = function() {
+                    eliminarValor(newInputGroup);
+                };
 
-                    // Añadir los elementos al div
-                    newInputGroup.appendChild(newInput);
-                    newInputGroup.appendChild(deleteButton);
+                // Añadir los elementos al div
+                newInputGroup.appendChild(newInput);
+                newInputGroup.appendChild(deleteButton);
 
-                    // Añadir el nuevo div al contenedor
-                    document.getElementById('cluster-container').appendChild(newInputGroup);
+                // Añadir el nuevo div al contenedor
+                document.getElementById('cluster-container').appendChild(newInputGroup);
 
-                    // Limpiar el input de búsqueda después de agregar
-                    document.getElementById('service').value = '';
-                } else {
-                    alert('Por favor, ingrese un valor para agregar.');
-                }
-            });
-        });
-
-        function eliminarValor(element) {
-            element.remove();
-        }
-
-        $(document).ready(function() {
-            var table = $('#example1').DataTable({
-                "language": {
-                    "sProcessing": "Procesando...",
-                    "sLengthMenu": "Mostrar MENU registros",
-                    "sZeroRecords": "No se encontraron resultados",
-                    "sEmptyTable": "Ningún dato disponible en esta tabla",
-                    "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                    "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                    "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-                    "sInfoPostFix": "",
-                    "sSearch": "Buscar:",
-                    "sUrl": "",
-                    "sInfoThousands": ",",
-                    "sLoadingRecords": "Cargando...",
-                    "oPaginate": {
-                        "sFirst": "Primero",
-                        "sLast": "Último",
-                        "sNext": "Siguiente",
-                        "sPrevious": "Anterior"
-                    },
-                    "oAria": {
-                        "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                    }
-                },
-                "order": [
-                    [0, "asc"]
-                ],
-                "search": {
-                    "smart": false //desactivar la busqueda inteligente para coincidencias exactas
-                }
-            });
-            // Agregar evento para la búsqueda
-            $('#service').on('keyup', function() {
-                var searchString = $(this).val().trim();
-
-                // Ocultar la tabla si no hay términos de búsqueda
-                if (searchString === '') {
-                    $('#example1').hide();
-                    return;
-                }
-
-                // Realizar la búsqueda y mostrar solo los resultados coincidentes
-                table.search(searchString).draw();
-
-                // Mostrar la tabla después de la búsqueda
-                $('#example1').show();
-            });
-
-            // Apply the search
-            table.columns().every(function() {
-                var that = this;
-
-                $('input', this.footer()).on('keyup change clear', function() {
-                    if (that.search() !== this.value) {
-                        that
-                            .search(this.value)
-                            .draw();
-                    }
-                });
-            });
-
-            // Cargar botones para acciones de exportar de datatables
-            var buttons = new $.fn.dataTable.Buttons(table, {
-                buttons: [{
-                    extend: 'copy',
-                    text: 'Copiar',
-                    className: 'btn btn-sm btn-default'
-                }, {
-                    extend: 'print',
-                    text: 'Imprimir',
-                    className: 'btn btn-sm btn-default'
-                }, {
-                    extend: 'collection',
-                    className: 'btn btn-sm btn-default',
-                    text: 'Exportar',
-                    buttons: [
-                        'csv',
-                        'excel',
-                        'pdf'
-                    ]
-                }]
-            }).container().appendTo('#btn_table');
-
-            // Mostrar la tabla al hacer clic en el botón de búsqueda
-            $('#ejecutar').on('click', function() {
-                $('#example1').show();
-            });
-        });
-
-        // Script para ejecutar el modal de confirmación de borrado
-        $('#confirm-delete').on('show.bs.modal', function(e) {
-            var formulario = createForm();
-
-            function createForm() {
-                var form = $('<form>', {
-                    'method': 'POST',
-                    'action': $(e.relatedTarget).data('href')
-                });
-
-                var token = $('<input>', {
-                    'type': 'hidden',
-                    'name': '_token',
-                    'value': '{{ csrf_token() }}'
-                });
-
-                var hiddenInput = $('<input>', {
-                    'name': '_method',
-                    'type': 'hidden',
-                    'value': 'DELETE'
-                });
-
-                return form.append(token, hiddenInput).appendTo('body');
+                // Limpiar el input de búsqueda después de agregar
+                document.getElementById('service').value = '';
+            } else {
+                alert('Por favor, ingrese un valor para agregar.');
             }
-            $(this).find('.btn-ok').on('click', function() {
-                formulario.submit();
-            });
+        });
+    });
+
+    function eliminarValor(element) {
+        element.remove();
+    }
+
+    $(document).ready(function() {
+        // Ocultar la tabla y la paginación inicialmente
+        $('#example1').hide();
+        $('.dataTables_paginate').hide();
+
+        var table = $('#example1').DataTable({
+            "language": {
+                "sProcessing": "Procesando...",
+                "sLengthMenu": "Mostrar MENU registros",
+                "sZeroRecords": "No se encontraron resultados",
+                "sEmptyTable": "Ningún dato disponible en esta tabla",
+                "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix": "",
+                "sSearch": "Buscar:",
+                "sUrl": "",
+                "sInfoThousands": ",",
+                "sLoadingRecords": "Cargando...",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast": "Último",
+                    "sNext": "Siguiente",
+                    "sPrevious": "Anterior"
+                },
+                "oAria": {
+                    "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                }
+            },
+            "order": [
+                [0, "asc"]
+            ],
+            "search": {
+                "smart": false // desactivar la búsqueda inteligente para coincidencias exactas
+            }
         });
 
-        function agregarValor(id, alias) {
-            // Verificar si el cluster ya está presente
-            var valoresContainer = document.getElementById('valores-container');
-            var alreadyAdded = false;
+        // Mostrar la tabla y la paginación al hacer clic en el botón de búsqueda
+        $('#ejecutar').on('click', function() {
+            var searchString = $('#service').val().trim();
 
-            clusterContainer.querySelectorAll('input[name="valores_agregados[][id]"]').forEach(function(input) {
-                if (input.value === id) {
-                    alreadyAdded = true;
+            if (searchString !== '') {
+                table.search(searchString).draw();
+                $('#example1').show();
+                $('.dataTables_paginate').show();
+            }
+        });
+
+        // Ocultar la tabla y la paginación si no hay términos de búsqueda en la entrada
+        $('#service').on('keyup', function() {
+            var searchString = $(this).val().trim();
+
+            if (searchString === '') {
+                $('#example1').hide();
+                $('.dataTables_paginate').hide();
+            }
+        });
+
+        // Cargar botones para acciones de exportar de datatables
+        var buttons = new $.fn.dataTable.Buttons(table, {
+            buttons: [{
+                extend: 'copy',
+                text: 'Copiar',
+                className: 'btn btn-sm btn-default'
+            }, {
+                extend: 'print',
+                text: 'Imprimir',
+                className: 'btn btn-sm btn-default'
+            }, {
+                extend: 'collection',
+                className: 'btn btn-sm btn-default',
+                text: 'Exportar',
+                buttons: [
+                    'csv',
+                    'excel',
+                    'pdf'
+                ]
+            }]
+        }).container().appendTo('#btn_table');
+
+        // Aplicar la búsqueda a cada columna
+        table.columns().every(function() {
+            var that = this;
+
+            $('input', this.footer()).on('keyup change clear', function() {
+                if (that.search() !== this.value) {
+                    that.search(this.value).draw();
                 }
             });
+        });
+    });
 
-            if (alreadyAdded) {
-                // Eliminar el modal existente si hay alguno
-                $('.modal').remove();
+    // Script para ejecutar el modal de confirmación de borrado
+    $('#confirm-delete').on('show.bs.modal', function(e) {
+        var formulario = createForm();
 
-                var modalHtml = `
+        function createForm() {
+            var form = $('<form>', {
+                'method': 'POST',
+                'action': $(e.relatedTarget).data('href')
+            });
+
+            var token = $('<input>', {
+                'type': 'hidden',
+                'name': '_token',
+                'value': '{{ csrf_token() }}'
+            });
+
+            var hiddenInput = $('<input>', {
+                'name': '_method',
+                'type': 'hidden',
+                'value': 'DELETE'
+            });
+
+            return form.append(token, hiddenInput).appendTo('body');
+        }
+        $(this).find('.btn-ok').on('click', function() {
+            formulario.submit();
+        });
+    });
+
+    function agregarValor(id, alias) {
+        // Verificar si el cluster ya está presente
+        var valoresContainer = document.getElementById('valores-container');
+        var alreadyAdded = false;
+
+        clusterContainer.querySelectorAll('input[name="valores_agregados[][id]"]').forEach(function(input) {
+            if (input.value === id) {
+                alreadyAdded = true;
+            }
+        });
+
+        if (alreadyAdded) {
+            // Eliminar el modal existente si hay alguno
+            $('.modal').remove();
+
+            var modalHtml = `
         <div class="modal" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
@@ -346,54 +348,55 @@
             </div>
         </div>`;
 
-                // Convertir el HTML en un elemento del DOM
-                var modalElement = document.createRange().createContextualFragment(modalHtml);
+            // Convertir el HTML en un elemento del DOM
+            var modalElement = document.createRange().createContextualFragment(modalHtml);
 
-                // Agregar el modal al cuerpo del documento
-                document.body.appendChild(modalElement);
+            // Agregar el modal al cuerpo del documento
+            document.body.appendChild(modalElement);
 
-                // Mostrar el modal
-                $('.modal').modal('show');
+            // Mostrar el modal
+            $('.modal').modal('show');
 
-                return;
-            }
-
-            // Si no está presente, agregarlo
-            var nuevoCriterioDiv = document.createElement('div');
-            nuevoCriterioDiv.classList.add('input-group');
-
-            var nuevoCriterioHidden = document.createElement('input');
-            nuevoCriterioHidden.name = 'valores_agregados[][id]';
-            nuevoCriterioHidden.type = 'hidden';
-            nuevoCriterioHidden.value = id;
-
-            var nuevoCriterio = document.createElement('input');
-            nuevoCriterio.name = 'valores_agregados[][visible]';
-            nuevoCriterio.classList.add('form-control');
-            nuevoCriterio.rows = 1;
-            nuevoCriterio.value = alias;
-            nuevoCriterio.id = id;
-            nuevoCriterio.disabled = true;
-
-            var eliminarCriterioBtn = document.createElement('button');
-            eliminarCriterioBtn.type = 'button';
-            eliminarCriterioBtn.classList.add('btn', 'btn-sm', 'btn-danger', 'ml-2');
-            eliminarCriterioBtn.textContent = 'Eliminar';
-            eliminarCriterioBtn.addEventListener('click', function() {
-                // Remover el div contenedor del botón eliminar
-                this.parentNode.parentNode.removeChild(this.parentNode);
-            });
-
-            nuevoCriterioDiv.appendChild(nuevoCriterioHidden);
-            nuevoCriterioDiv.appendChild(nuevoCriterio);
-            nuevoCriterioDiv.appendChild(eliminarCriterioBtn);
-            clusterContainer.appendChild(nuevoCriterioDiv);
+            return;
         }
 
-        function eliminarCluster(id) {
-            // Eliminar el div contenedor del cluster
-            var divToRemove = document.getElementById(id).parentNode;
-            divToRemove.parentNode.removeChild(divToRemove);
-        }
-    </script>
+        // Si no está presente, agregarlo
+        var nuevoCriterioDiv = document.createElement('div');
+        nuevoCriterioDiv.classList.add('input-group');
+
+        var nuevoCriterioHidden = document.createElement('input');
+        nuevoCriterioHidden.name = 'valores_agregados[][id]';
+        nuevoCriterioHidden.type = 'hidden';
+        nuevoCriterioHidden.value = id;
+
+        var nuevoCriterio = document.createElement('input');
+        nuevoCriterio.name = 'valores_agregados[][visible]';
+        nuevoCriterio.classList.add('form-control');
+        nuevoCriterio.rows = 1;
+        nuevoCriterio.value = alias;
+        nuevoCriterio.id = id;
+        nuevoCriterio.disabled = true;
+
+        var eliminarCriterioBtn = document.createElement('button');
+        eliminarCriterioBtn.type = 'button';
+        eliminarCriterioBtn.classList.add('btn', 'btn-sm', 'btn-danger', 'ml-2');
+        eliminarCriterioBtn.textContent = 'Eliminar';
+        eliminarCriterioBtn.addEventListener('click', function() {
+            // Remover el div contenedor del botón eliminar
+            this.parentNode.parentNode.removeChild(this.parentNode);
+        });
+
+        nuevoCriterioDiv.appendChild(nuevoCriterioHidden);
+        nuevoCriterioDiv.appendChild(nuevoCriterio);
+        nuevoCriterioDiv.appendChild(eliminarCriterioBtn);
+        clusterContainer.appendChild(nuevoCriterioDiv);
+    }
+
+    function eliminarCluster(id) {
+        // Eliminar el div contenedor del cluster
+        var divToRemove = document.getElementById(id).parentNode;
+        divToRemove.parentNode.removeChild(divToRemove);
+    }
+</script>
+
 @stop
